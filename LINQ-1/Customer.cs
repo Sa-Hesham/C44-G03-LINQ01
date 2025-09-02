a6a4ae5666e5bc39e290851796e149343da43c24
Sa-Hesham <elsayedheshamdarwish@gmail.com>
2025-09-02 18:18:01 +0300

    Restriction Operators

---
+++ LINQ-1/Customer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_1
+{
+    internal class Customer
+    {
+        public string CustomerID { get; set; } = string.Empty;
+        public string CustomerName { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+        public string City { get; set; } = string.Empty;
+        public string Region { get; set; } = string.Empty;
+        public string PostalCode { get; set; } = string.Empty;
+        public string Country { get; set; } = string.Empty;
+        public string Phone { get; set; } = string.Empty;
+        public string Fax { get; set; } = string.Empty;
+        public Order[] Orders { get; set; } = new Order[10];
+        public Customer(string customerID, string customerName)
+        {
+            CustomerID = customerID;
+            CustomerName = customerName;
+            Orders = new Order[10];
+        }
+        public Customer() { }
+
+        public override string ToString()
+            => $"{CustomerID}, {CustomerName}, {Address}, {City}, {Region}, {PostalCode}, {Country}, {Phone}, {Fax}";
+    }
+}
+
--- LINQ-1/Program.cs
+++ LINQ-1/Program.cs
@@ -1,10 +1,50 @@
-﻿namespace LINQ_1
+﻿
+
+using System.Runtime.ConstrainedExecution;
+using System.Xml.Linq;
+using static LINQ_1.ListGenerator;
+
+
+namespace LINQ_1
+  
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region LINQ - Restriction Operators
+
+            // 1.Find all products that are out of stock.
+
+
+            //var result= Products.Where(p => p.UnitsInStock == 0);
+            // foreach (var r in result) {
+
+            //     Console.WriteLine(r);
+            // }
+
+            //2.Find all products that are in stock and cost more than 3.00 per unit.
+
+            //var result = Products.Where(p => p.UnitPrice>3.00M);
+            //foreach (var r in result)
+            //{
+
+            //    Console.WriteLine(r);
+            //}
+
+            //  3.Returns digits whose name is shorter than their value.
+            //String[] Arr = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+
+
+            //var res = Arr.Select((name, vlaue) => new { Name = name, Value = vlaue })
+            //    .Where(p => p.Name.Length < p.Value);
+            //foreach (var item in res) {
+
+            //    Console.WriteLine(item);
+            //}
+
+
+            #endregion
         }
     }
 }
